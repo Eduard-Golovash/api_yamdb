5d679866d816b2c8ac95abd6c3985fc0dbcd20de
Павел Никаноров <teddi.xd@yandex.ru>
2023-06-14 11:25:13 +0300

    Startapp api and reviews. Add model Review, Comment + serealizers

---
+++ api_yamdb/api/serializer.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from reviews.models import Review, Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        model = Review
+        read_only_fields = ('title',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_date')
+        model = Comment
+        read_only_fields = ('review',)
---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE,
+        verbose_name='Произведение',
+        related_name='rewiews'
+    )
+    text = models.TextField(
+        verbose_name='Отзыв',
+        help_text='Текст отзыва'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        verbose_name='Автор',
+        related_name='reviews'
+    )
+    score = models.IntegerField(
+        null=True,
+        default=None,
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_review',
+            ),
+        )
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               verbose_name='Автор',
+                               related_name='comments')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE,
+                               verbose_name='Отзыв',
+                               related_name='comments')
+    text = models.TextField(verbose_name='Комментарий',
+                            help_text='Текст комментария')
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True, db_index=True)
+
+    def __str__(self) -> str:
+        return self.text
