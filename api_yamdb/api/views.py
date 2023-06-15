77d04f22a83ae07ba69c210fd0c0b2f71a2053f3
Павел Никаноров <teddi.xd@yandex.ru>
2023-06-15 16:26:08 +0300

    Добавил VIew Отзывы и Комментарии

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,1 +1,49 @@
-from django.shortcuts import render
+from rest_framework.exceptions import ParseError
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from reviews.models import Comment, Review, Title
+from .serializer import ReviewSerializer, CommentSerializer
+from .permissions import (IsAdminOrModeratorOrOwnerOrReadOnly, AdminOrReadOnly,
+                          IsAdmin)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAdminOrModeratorOrOwnerOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        queryset = Review.objects.filter(title=title_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        if Review.objects.filter(
+            title=title,
+            author=self.request.user
+        ).exists():
+            raise ParseError
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (IsAdminOrModeratorOrOwnerOrReadOnly,)
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Review,
+            title_id=self.kwargs.get('title_id'),
+            pk=self.kwargs.get('review_id')
+        )
+        return review.comments.all().order_by('id')
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(
+            Review,
+            title_id=self.kwargs.get('title_id'),
+            pk=self.kwargs.get('review_id')
+        )
+        serializer.save(author=self.request.user, review=review)
