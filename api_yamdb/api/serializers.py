8cde97182872fce30fe5fcc3971d51f110b13d0b
Eduard Golovash <myausek@gmail.com>
2023-06-25 13:20:15 +0400

    review-2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
@@ -22,16 +21,6 @@ class ReviewSerializer(serializers.ModelSerializer):
         ):
             raise serializers.ValidationError("Entry already exists.")
         return data
-
-    def create(self, validated_data):
-        request = self.context["request"]
-        title_id = self.context["view"].kwargs.get("title_id")
-        title = get_object_or_404(Title, pk=title_id)
-        if Review.objects.filter(title=title, author=request.user).exists():
-            raise serializers.ValidationError("Entry already exists.")
-        validated_data["author"] = request.user
-        validated_data["title"] = title
-        return super().create(validated_data)
 
 
 class CommentSerializer(serializers.ModelSerializer):
